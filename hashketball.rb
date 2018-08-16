858989517016accb63b38dc6710abb3c3ada401d
Christopher Stull <chris.stull@gmail.com>
2018-08-16 18:38:26 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -34,7 +34,7 @@ def game_hash
           :blocks => 1,
           :slam_dunks => 15
         },
-        "Mason Plumee" => {
+        "Mason Plumlee" => {
           :number => 1,
           :shoe => 19,
           :points => 26,
